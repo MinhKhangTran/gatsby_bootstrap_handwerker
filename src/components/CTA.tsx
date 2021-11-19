eacf1b40c5e4d58d54074081f9c0adb465735fb4
MinhKhangTran <minhkhang.tran@gmx.de>
2021-11-19 06:00:19 +0100

    :sparkles: cta

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,7 +1,44 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section id="cta" className="section">
+      <Container>
+        <Row className="align-items-center">
+          <Col md={6} className="order-md-2">
+            <StaticImage
+              src="../images/cta.jpg"
+              alt="cta image - anrufender mann"
+            />
+          </Col>
+          <Col md={6}>
+            <h2 className="text-primary mt-3">Bereit für einen Anruf?</h2>
+            <p className="lead text-secondary">
+              Meow meow mama lick arm hair yet lick human with sandpaper tongue
+              but if human is on laptop
+            </p>
+            <article className="d-flex gap-3">
+              <div>
+                <FaPhoneAlt className="text-primary" size="2em"></FaPhoneAlt>
+                <span className="visually-hidden">Phone</span>
+              </div>
+              <p className="text-primary">123 4567 890</p>
+            </article>
+            <article className="d-flex gap-3 mt-3">
+              <div>
+                <FaEnvelope className="text-primary" size="2em"></FaEnvelope>
+                <span className="visually-hidden">E-Mail</span>
+              </div>
+              <p className="text-primary">handwerker@example.de</p>
+            </article>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default CTA;
