6e691bb863717f053429c4911e2f71920ca10e35
MinhKhangTran <minhkhang.tran@gmx.de>
2021-11-19 06:30:24 +0100

    :sparkles: start with zeitstrahl

--- src/components/Zeitstrahl.tsx
+++ src/components/Zeitstrahl.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 const Zeitstrahl = () => {
-  return <div>Zeitstrahl</div>;
+  return (
+    <section id="zeitstrahl" className="mt-4">
+      <Container>
+        <article>
+          <div className="d-flex gap-2">
+            <div className="punkt"></div>
+            <p className="text-primary">1993</p>
+          </div>
+        </article>
+      </Container>
+    </section>
+  );
 };
 
 export default Zeitstrahl;
